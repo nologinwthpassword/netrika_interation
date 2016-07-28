7fd1c855cdacba77294592c37f43eaaf82d9464e
А А < >
2016-07-28 12:22:02 +0300

    closing first connection to posgtres

--- src/ru/rtlabs/Main.java
+++ src/ru/rtlabs/Main.java
@@ -4,15 +4,23 @@ import ru.rtlabs.DB.DBWorker;
 import ru.rtlabs.DB.DBWorker2;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
         DBWorker connection = new DBWorker();
-        DBWorker2 connection2 = new DBWorker2();
+
         WebSession webSession = new WebSession();
         webSession.getUid(connection);
+        try {
+            connection.getConnection().close();
+            Thread.sleep(100);
+        } catch (SQLException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        DBWorker2 connection2 = new DBWorker2();
         SoapSender sender = new SoapSender(webSession.getWebSessionUid());
         sender.parse(connection2);
 
